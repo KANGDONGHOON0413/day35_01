07f7a0441a0e55a14c549aae81ec1ec9604db242
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-28 23:29:27 +0900

    초기 커밋.

---
+++ day35_01/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace day35_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BTN_Call = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(119, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(515, 269);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // BTN_Call
+            // 
+            this.BTN_Call.Location = new System.Drawing.Point(326, 309);
+            this.BTN_Call.Name = "BTN_Call";
+            this.BTN_Call.Size = new System.Drawing.Size(94, 29);
+            this.BTN_Call.TabIndex = 1;
+            this.BTN_Call.Text = "불러오기";
+            this.BTN_Call.UseVisualStyleBackColor = true;
+            this.BTN_Call.Click += new System.EventHandler(this.BTN_Call_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTN_Call);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BTN_Call;
+    }
+}
+
---
+++ day35_01/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace day35_01
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter DataA;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = "server=.\\SQLEXPRESS;database=test; user id=sa; pwd=alencia;";
+        }
+
+        private void BTN_Call_Click(object sender, EventArgs e)
+        {
+            DataA = new SqlDataAdapter("SELECT * FROM testTable01", conn);
+            DataSet DS01 = new DataSet();
+            DataA.Fill(DS01, "testTable01");
+            dataGridView1.DataSource = DS01.Tables[0];
+        }
+
+       
+    }
+}
